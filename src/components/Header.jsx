f4601b808dc93df1fb9f21210d4f4659ef6cf358
leogon1234 <leandrogv2105@gmail.com>
2025-10-19 18:05:05 -0300

    arreglar responsividad
    se arreglo la responsividad

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,6 +22,18 @@ export default function Header() {
           <span className="fw-bold fs-5 text-white">LimpiFresh</span>
         </Link>
 
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
         {/* CENTRO DEL HEADER */}
         <div className="collapse navbar-collapse" id="navbarContent">
           <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
