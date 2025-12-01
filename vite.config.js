d12938af353994e27e3b8c078cec30d473bbe522
leogon1234 <leandrogv2105@gmail.com>
2025-12-01 00:57:59 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/limpifresh_pages",
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.js',
-    base: "/limpifresh_pages",
   },
 })
 
